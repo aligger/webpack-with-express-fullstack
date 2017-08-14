2020d4ac6a63c0325b5c9ce0da7a451fd3ddef87
aligger <cestrmail@163.com>
2017-08-14 14:41:19 +0800

    提交代码

--- server/app.js
+++ server/app.js
@@ -4,10 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+import routes from './routes/routes'
 var app = express();
 
 // view engine setup
@@ -21,9 +18,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+routes(app);
 
-app.use('/', index);
-app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/routes/routes.js
@@ -0,0 +1,12 @@
+const index = require('./index');
+const users = require('./users');
+const alertRouter = require('./users2');
+
+// 路由
+// const routes = function (app) {
+//     app.use('/', index);
+//     app.use('/users', users);
+//     app.use('/users2', alertRouter);
+// };
+
+module.exports = routes;
